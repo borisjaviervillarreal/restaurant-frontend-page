a854c0e7e479a83efa3064beb2c991d42b4be3cd
Javier Villarreal <javier.villarreal.623@gmail.com>
2016-10-24 16:44:48 -0500

    Menu Animation finished

--- js/main.js
+++ js/main.js
@@ -2,9 +2,37 @@
 
 	let sticky = false
 
+	$('#sticky-navigation').removeClass('hidden')
+	$('#sticky-navigation').slideUp(0)
+
 	$(window).scroll(function(){
-		console.log("Scroll")
+		const inBottom = isInBottom()
+
+		if(inBottom && !sticky){
+			//Mostrar la navegacion
+			sticky = true
+			stickyNavigation()
+
+		}
+
+		if(!inBottom && sticky){
+			//Ocultar la navegacion sticky
+			sticky = false
+			unStickNavigation()
+		}
 	})
+
+	function stickyNavigation(){
+		$('#description').addClass('fixed').removeClass('absolute')
+		$('#navigation').slideUp('fast')
+		$('#sticky-navigation').slideDown('fast')
+	}
+
+	function unStickNavigation(){
+		$('#description').removeClass('fixed').addClass('absolute')
+		$('#navigation').slideDown('fast')
+		$('#sticky-navigation').slideUp('fast')
+	}
 
 	function isInBottom(){
 
@@ -12,7 +40,7 @@
 
 		const descriptionHeight = $description.height()
 
-		return $(window).scrollTop()
+		return $(window).scrollTop() > $(window).height() - (descriptionHeight * 2)
 
 	}
 
