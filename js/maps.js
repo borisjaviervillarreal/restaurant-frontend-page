1b838d9d2c24a7c0f62451f4670fb4779d13200c
Javier Villarreal <javier.villarreal.623@gmail.com>
2016-10-26 13:52:27 -0500

    User Location obtained by Google Maps API and JS

--- js/maps.js
+++ js/maps.js
@@ -1,17 +1,38 @@
 ;(function (){
 
+	class UserLocation{
+		static get(callback){
+
+			//Verificar si el navegador tiene geolocalizacion
+			if(navigator.geolocation){
+				navigator.geolocation.getCurrentPosition((location)=>{
+					callback({
+						lat: location.coords.latitude,
+						lng: location.coords.longitude
+					})
+				})
+			}else{
+				alert("No pudimos detectar tu ubicacion")
+			}
+		}
+	}
+
 	//Cargar API de google maps
 	google.maps.event.addDomListener(window,'load',()=>{
 		const map = new google.maps.Map(
 			document.getElementById('map'),
 			{
 				center: {
-					lat: -0.180653,
-					lng: -78.467838
+					lat: -0.2576842,
+					lng: -78.5509023
 				},
-				zoom: 13
+				zoom: 14
 			}
 		)
+
+		UserLocation.get((coords)=>{
+			console.log(coords)
+		})
 	})
 
 })()
